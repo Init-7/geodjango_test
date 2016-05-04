b938b1ede3bc0db8e02274f93b4fa76a4d609cc0
jlautarosilva <jlautarosilva@gmail.com>
2016-05-04 01:41:56 -0300

    movido cv a app est y eliminadas imagenes de prueba

--- est/views.py
+++ est/views.py
@@ -1,3 +1,22 @@
 from django.shortcuts import render
 
-# Create your views here.
+from est.models import Planta, Zona, Trabajador, CentroNegocios
+
+def curriculum(request, trabajador):
+
+    data = Trabajador.objects.get(id=trabajador)
+
+    context = {'data': data}
+
+    return render(request,'cv/cv.html', context)
+
+#    return render(request, '../templates/curriculum/classic.html', {
+#        'resume': resume,
+#        'skills': resume.skills.order_by('category', '-weight'),
+#        'projects': resume.projects.order_by('-weight'),
+#        'experiences': resume.experiences.order_by('-start_year'),
+#        'trainings': resume.trainings.order_by('-year', '-month'),
+#        'certifications': resume.certifications.order_by('-start_year', '-start_month')
+#    })
+#
+#
--- geodjango_test/urls.py
+++ geodjango_test/urls.py
@@ -19,5 +19,5 @@ from django.contrib import admin
 urlpatterns = [
     url(r'^admin/', admin.site.urls),
     url(r'^gps/', include('gps.urls')),
-    url(r'^cv/', include('cv.urls')),
+    url(r'^est/', include('est.urls')),
 ]
