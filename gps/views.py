4d50e62324209069ef52502e24db0c99005d0151
rsantibanez <rodrigonicolas.sc@gmail.com>
2018-12-10 16:50:29 -0300

    centro3 without name

--- gps/views.py
+++ gps/views.py
@@ -300,7 +300,7 @@ def centro3(request):
                 z =Zona.objects.filter(zona__bbcontains=tp).last().nombre
                 if (z != t.last_z):
                     # msg = "AVISO: Trabajador %s %s Ingreso a zona: %s Nivel riesgo: %s Supervisor: %s %s %s. Monitorear en: http://www.staff.estchile.cl/gps/sms/%s" % (t.primer_nombre, t.apellidop, z, t.nivel_riesgo, t.supervisor.primer_nombre, t.supervisor.apellidop, t.supervisor.fono, td.fono_gps)
-                    msg = "AVISO: Trabajador %s %s Ingreso a zona: %s Nivel riesgo: %s Supervisor: %s %s %s. Monitorear en: http://www.staff.estchile.cl/gps/sms/%s" % (t.primer_nombre, t.apellidop, z, t.nivel_riesgo, 'NAME', 'LASTNAME', t.supervisor.fono, td.fono_gps)
+                    msg = "AVISO: Trabajador %s %s Ingreso a zona: %s Nivel riesgo: %s Supervisor: %s %s %s. Monitorear en: http://www.staff.estchile.cl/gps/sms/%s" % (t.primer_nombre, t.apellidop, z, t.nivel_riesgo, 'NAME', 'LASTNAME', '010101', 'gp02')
                     sms_twilio_z(msg)
                     t.last_z = z
                 else:
