634d7845107d4b83ebf0418c74ddfc6e903388b7
rsantibanez <rodrigonicolas.sc@gmail.com>
2018-12-10 11:30:46 -0300

    upd

--- gps/views.py
+++ gps/views.py
@@ -19,9 +19,9 @@ from django.core.serializers.json import DjangoJSONEncoder
 from django.core.serializers.python import Serializer
 from datetime import timedelta
 from django.views.decorators.csrf import ensure_csrf_cookie
-# from django_twilio.decorators import twilio_view
-# from twilio.twiml import Response
-# from twilio.rest import TwilioRestClient
+from django_twilio.decorators import twilio_view
+from twilio.twiml import Response
+from twilio.rest import TwilioRestClient
 from django.http import JsonResponse
 
 
