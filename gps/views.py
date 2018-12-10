88a158b3495ef5fa9aadd4bf1a07b636846c7dfb
rsantibanez <rodrigonicolas.sc@gmail.com>
2018-12-10 14:55:48 -0300

    update add comment http

--- gps/views.py
+++ gps/views.py
@@ -21,7 +21,6 @@ from datetime import timedelta
 from django.views.decorators.csrf import ensure_csrf_cookie
 from django_twilio.decorators import twilio_view
 # from twilio.twiml import Response
-from twilio.http import HttpResponse
 from twilio.rest import TwilioRestClient
 from django.http import JsonResponse
 
