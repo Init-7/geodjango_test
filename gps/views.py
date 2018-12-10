224c3480fa18edfbb3f8a5e5d905d6bb8e335c40
rsantibanez <rodrigonicolas.sc@gmail.com>
2018-12-10 17:23:01 -0300

    twilio8

--- gps/views.py
+++ gps/views.py
@@ -826,7 +826,7 @@ def sms_twilio(request):
     return m
 
 @twilio_view
-def sms_twilio_z(msg):
+def sms_twilio_z(msg = ''):
    print(TWILIO_ACCOUNT_SID)
    client = TwilioRestClient(TWILIO_ACCOUNT_SID, TWILIO_AUTH_TOKEN)
    client.messages.create(from_="+56226660685", to="+56956711890", body=msg)
