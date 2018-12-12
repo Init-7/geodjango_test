0a1fdadfd1adc2f07fc4a823708abd2ef20011d8
rsantibanez <rodrigonicolas.sc@gmail.com>
2018-12-12 14:45:04 -0300

    update sms

--- gps/views.py
+++ gps/views.py
@@ -314,15 +314,22 @@ def centro3(request):
 
                     # url='https://api.connectus.cl/api_v1/send_sms'
 
-                    CONNETCTUS_URL ='https://api.connectus.cl/api_v1/send_sms'
-                    CONNECTUS_ACCOUNT_SID = '9e01f41122384f5ea9192ade9d1c1c0c'
-                    CONNECTUS_AUTH_TOKEN = '4ba4d39de3c14a8099c4b5cf0a1cab19'
+                    # CONNETCTUS_URL ='https://api.connectus.cl/api_v1/send_sms'
+                    # CONNECTUS_ACCOUNT_SID = '9e01f41122384f5ea9192ade9d1c1c0c'
+                    # CONNECTUS_AUTH_TOKEN = '4ba4d39de3c14a8099c4b5cf0a1cab19'
+                    #
+                    # params = dict()
+                    # params['dst_number']=5685567407
+                    # params['sms_content']='Hola Mundo!'
+                    #
+                    # response = requests.post(CONNETCTUS_URL, params = params, auth=(CONNECTUS_ACCOUNT_SID,CONNECTUS_ACCOUNT_SID))
 
+                    # import requests
+                    url = 'https://api.connectus.cl/api_v1/send_sms'
                     params = dict()
-                    params['dst_number']=5685567407
+                    params['dst_number']=56956711890
                     params['sms_content']='Hola Mundo!'
-
-                    response = requests.post(CONNETCTUS_URL, params = params, auth=(CONNECTUS_ACCOUNT_SID,CONNECTUS_ACCOUNT_SID))
+                    response = requests.post(url, params=params, auth=(CONNECTUS_ACCOUNT_SID, CONNECTUS_AUTH_TOKEN))
 
                     print response
 
--- test.py
+++ test.py
@@ -1,11 +1,6 @@
-CONNETCTUS_URL ='https://api.connectus.cl/api_v1/send_sms'
-CONNECTUS_ACCOUNT_SID = '9e01f41122384f5ea9192ade9d1c1c0c'
-CONNECTUS_AUTH_TOKEN = '4ba4d39de3c14a8099c4b5cf0a1cab19'
-
+import requests
+url = 'https://api.connectus.cl/api_v1/send_sms'
 params = dict()
-params['dst_number']=5685567407
-params['sms_content']='Hola Mundo!'
-
-response = requests.post(CONNETCTUS_URL, params = params, auth=(CONNECTUS_ACCOUNT_SID,CONNECTUS_ACCOUNT_SID))
-
-print response
+params['dst_number'] = 5685567407
+params['sms_content'] = 'Con Connectus es muy facil integrar aplicaciones.'
+response = requests.post(url, params=params, auth=('9e01f41122384f5ea9192ade9d1c1c0c', '4ba4d39de3c14a8099c4b5cf0a1cab19'))
