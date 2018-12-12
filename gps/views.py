2f7639766fb1687806fa4fcb158c0e8788683a30
rsantibanez <rodrigonicolas.sc@gmail.com>
2018-12-12 14:27:23 -0300

    update sms

--- gps/views.py
+++ gps/views.py
@@ -319,7 +319,7 @@ def centro3(request):
                     CONNECTUS_AUTH_TOKEN = '4ba4d39de3c14a8099c4b5cf0a1cab19'
 
                     params = dict()
-                    params['dst_number']=56711890
+                    params['dst_number']=5685567407
                     params['sms_content']='Hola Mundo!'
 
                     response = requests.post(CONNETCTUS_URL, params = params, auth=(CONNECTUS_ACCOUNT_SID,CONNECTUS_ACCOUNT_SID))
