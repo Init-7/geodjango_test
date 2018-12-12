3bf12f4b8e250ea518dfe976a2b999099c9cb4d8
rsantibanez <rodrigonicolas.sc@gmail.com>
2018-12-12 14:46:24 -0300

    update sms

--- gps/views.py
+++ gps/views.py
@@ -315,8 +315,8 @@ def centro3(request):
                     # url='https://api.connectus.cl/api_v1/send_sms'
 
                     # CONNETCTUS_URL ='https://api.connectus.cl/api_v1/send_sms'
-                    # CONNECTUS_ACCOUNT_SID = '9e01f41122384f5ea9192ade9d1c1c0c'
-                    # CONNECTUS_AUTH_TOKEN = '4ba4d39de3c14a8099c4b5cf0a1cab19'
+                    CONNECTUS_ACCOUNT_SID = '9e01f41122384f5ea9192ade9d1c1c0c'
+                    CONNECTUS_AUTH_TOKEN = '4ba4d39de3c14a8099c4b5cf0a1cab19'
                     #
                     # params = dict()
                     # params['dst_number']=5685567407
