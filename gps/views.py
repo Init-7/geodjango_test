d16b0336d977122000892f6f57607624a8314d28
rsantibanez <rodrigonicolas.sc@gmail.com>
2018-12-12 15:16:00 -0300

    update sms

--- gps/views.py
+++ gps/views.py
@@ -810,7 +810,7 @@ def trabajador_z_riesgo(request, planta):
 
 # @app.route('/webhooks/inbound-sms', methods=['GET', 'POST'])
 def sms_connectus(request):
-    name = request.POST.get('from', '')
+    # name = request.POST.get('from', '')
     # msg = 'Se ha recibido un mensaje SOS dirijase a http://staff.estchile.cl/sms/%s/ para ver las alertas o a http://staff.estchile.cl/est/cv/%s/ para ver su ficha' % (name)
     msg = 'def sms_connectus'
     r = Response()
