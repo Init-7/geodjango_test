10932ee682072962b6b986736867923c90372f7a
rsantibanez <rodrigonicola.sc@gmail.com>
2018-12-13 17:33:17 +0000

    aaa

--- geodjango_test/settings.py
+++ geodjango_test/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'hs*0gc=u()ty8s1&nayv0&ux8@ny0q2n8l@5omz@4ai1c5t%@_'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['18.228.128.227', '172.31.7.147', 'staff.estchile.cl']
+ALLOWED_HOSTS = ['127.0.0.1', 'localhost', '18.228.128.227', '172.31.7.147', 'staff.estchile.cl', '[::1]']
 
 
 # Application definition
