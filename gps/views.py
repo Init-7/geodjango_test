0034dcb2cea1cafe234e15f70edb5b14184722b0
rsantibanez <rodrigonicolas.sc@gmail.com>
2018-12-13 12:46:16 -0300

    sms

--- gps/views.py
+++ gps/views.py
@@ -815,8 +815,9 @@ def sms_connectus(request):
     print 'Method: '+request.method
 
     print '===0==='
-    data = request.POST.get('id')
-    print data
+    # data = request.POST.get('id')
+    # print data
+    print request.json()
     print '===0==='
 
     # msg = 'Se ha recibido un mensaje SOS dirijase a http://staff.estchile.cl/sms/%s/ para ver las alertas o a http://staff.estchile.cl/est/cv/%s/ para ver su ficha' % (name)
