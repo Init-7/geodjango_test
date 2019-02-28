b1e8c272dfb58b6ac379495a21d55c04d801a96a
rsantibanez <rodrigonicolas.sc@gmail.com>
2019-02-28 00:43:07 -0300

    update

--- est/urls.py
+++ est/urls.py
@@ -4,6 +4,6 @@ from . import views
 
 urlpatterns = [
     url(r'^cv/(?P<trabajador>[0-9]+)/$' ,views.curriculum, name='curriculum'),
-    url(r'^machine/(?P<trabajador>[0-9]+)/$' ,views.curriculum, name='machine'),
+    url(r'^machine/(?P<trabajador>[0-9]+)/$' ,views.machine, name='machine'),
     url(r'^card/(?P<trabajador>[0-9]+)/$' ,views.card, name='card'),
 ]
